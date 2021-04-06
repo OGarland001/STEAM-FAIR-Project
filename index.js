c456b27a6ef1769b76ef8a690779478c1c2bf20b
kivi16 <76027650+kivi16@users.noreply.github.com>
2021-04-06 08:40:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+
 import Blockly from 'blockly'
+ //boy block
+ function boyblock(){
+ Blockly.Blocks['bgenderblock_type'] = {
+  init: function() {
+    this.appendStatementInput("Boy Alien")
+        .setCheck(null)
+        .appendField("Boy Alien");
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['bgenderblock_type'] = function(block) {
+  var statements_boy_alien = Blockly.JavaScript.statementToCode(block, 'Boy Alien');
+  // TODO: Assemble JavaScript into code variable.
+  var code = document.getElementById('Aliengen').innerHTML = '<img width="100" height="100" src="shrek.jpg">';
+  return code;
+};
+
+  function runCode() {
+    window.LoopTrap = 1000;
+      Blockly.JavaScript.INFINITE_LOOP_TRAP =
+          'if (--window.LoopTrap == 0) throw "Infinite loop.";\n';
+      var code = Blockly.JavaScript.workspaceToCode(workspace);
+      Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+      try {
+        eval(code);
+      } catch (e) {
+      }
+    }
+ }
+
